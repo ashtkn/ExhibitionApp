4aa3ef24d15c6e66999b3726e5476ea48663be51
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2019-10-15 15:11:45 +0900

    Viewのセットアップ処理を分離

--- ExhibitionApp/Views/SharingViewController/SharingViewController.swift
+++ ExhibitionApp/Views/SharingViewController/SharingViewController.swift
@@ -4,11 +4,10 @@ import SnapKit
 class SharingViewController: UIViewController {
     
     // MARK: Outlets
-    private weak var container: UIView!
     private weak var imageView: UIImageView!
     private weak var shareButton: UIButton!
     private weak var backButton: UIButton!
-    private weak var saveButton: UIButton!
+    private weak var saveSnapshotButton: UIButton!
 
     private var viewModel: SharingViewModel?
     
@@ -21,114 +20,29 @@ class SharingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // First, setup container
-        setupContainer()
-        
-        // Second, setup components
-        setupImageView()
-        setupShareButton()
-        setupBackButton()
-        setupSaveButton()
-    }
-    
-    private func setupContainer() {
-        let container = UIView()
+        var container = UIView()
         self.view.addSubview(container)
         
-        let top = self.view.safeArea.top
-        let bottom = self.view.safeArea.bottom
-        let leading = self.view.safeArea.leading
-        let trailing = self.view.safeArea.trailing
-        
+        let safeArea = self.view.safeArea
         container.snp.makeConstraints { make in
-            make.top.equalTo(top)
-            make.bottom.equalTo(bottom)
-            make.leading.equalTo(leading)
-            make.trailing.equalTo(trailing)
+            make.top.equalTo(safeArea.top)
+            make.bottom.equalTo(safeArea.bottom)
+            make.leading.equalTo(safeArea.leading)
+            make.trailing.equalTo(safeArea.trailing)
         }
         
-        self.container = container
-    }
-    
-    private func setupImageView() {
-        let imageView = UIImageView()
-        container.addSubview(imageView)
-        
+        imageView = SharingViewController.addImageView(&container)
         imageView.image = viewModel?.snapshotImage
-        imageView.backgroundColor = .blue
         
-        // NOTE: ImageViewはデフォルトで `isUserInteractionEnabled == false` なので，UIButtonを子にしても動きません．
-        // imageView.isUserInteractionEnabled = true
-        
-        imageView.snp.makeConstraints { make in
-            // NOTE: この制約は実現できません
-            // let ratio: CGFloat = 16/9
-            // make.top.equalToSuperview()
-            // make.width.equalToSuperview()
-            // make.height.equalTo(imageView.snp.width).multipliedBy(ratio)
-            make.top.bottom.leading.trailing.equalToSuperview()
-        }
-        
-        self.imageView = imageView
-    }
-    
-    private func setupShareButton() {
-        let shareButton = UIButton()
-        container.addSubview(shareButton)
-        
-        shareButton.backgroundColor = .yellow
-        shareButton.layer.cornerRadius = 20
+        shareButton = SharingViewController.addShareButton(&container)
         shareButton.setTitle("シェア", for: .normal)
-        shareButton.setTitleColor(.black, for: .normal)
-        shareButton.titleLabel?.font = UIFont.mainFont(ofSize: 14)
-        shareButton.titleLabel?.textAlignment = .center
-        shareButton.contentHorizontalAlignment = .center
-        shareButton.contentVerticalAlignment = .center
-        
         shareButton.addTarget(self, action: #selector(didShareButtonTapped(_:)), for: .touchUpInside)
         
-        shareButton.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.width.equalToSuperview().inset(40)
-            make.height.equalTo(40)
-            make.bottom.equalToSuperview().offset(-20)
-        }
-        
-        self.shareButton = shareButton
-    }
-    
-    private func setupBackButton() {
-        let backButton = UIButton()
-        container.addSubview(backButton)
-        
-        let image = UIImage(named: "outline_keyboard_arrow_left_white_36pt_1x")
-        backButton.setImage(image, for: .normal)
+        backButton = SharingViewController.addBackButton(&container)
         backButton.addTarget(self, action: #selector(didBackButtonTapped(_:)), for: .touchUpInside)
         
-        backButton.snp.makeConstraints { make in
-            make.width.height.equalTo(36)
-            make.top.equalToSuperview().offset(18)
-            make.left.equalToSuperview().offset(18)
-        }
-        
-        self.backButton = backButton
-    }
-    
-    private func setupSaveButton() {
-        let saveButton = UIButton()
-        container.addSubview(saveButton)
-        
-        let image = UIImage(named: "baseline_save_alt_white_36pt_1x")
-        saveButton.setImage(image, for: .normal)
-        saveButton.addTarget(self, action: #selector(didSaveButtonTapped(_:)), for: .touchUpInside)
-        
-        saveButton.snp.makeConstraints { make in
-            make.width.height.equalTo(36)
-            make.top.equalToSuperview().offset(18)
-            make.right.equalToSuperview().offset(-18)
-        }
-        
-        self.saveButton = saveButton
+        saveSnapshotButton = SharingViewController.addSaveButton(&container)
+        saveSnapshotButton.addTarget(self, action: #selector(didSaveSnapshotButtonTapped(_:)), for: .touchUpInside)
     }
     
     // MARK: Actions
@@ -156,7 +70,89 @@ class SharingViewController: UIViewController {
         }
     }
     
-    @objc private func didSaveButtonTapped(_ sender: UIButton) {
+    @objc private func didSaveSnapshotButtonTapped(_ sender: UIButton) {
         print("Save Image")
+    }
+}
+
+// MARK: - Views
+
+extension SharingViewController {
+    
+    private static func addImageView(_ containerView: inout UIView) -> UIImageView {
+        let imageView = UIImageView()
+        containerView.addSubview(imageView)
+        
+        // For debug
+        imageView.backgroundColor = .blue
+        
+        // NOTE: ImageViewはデフォルトで `isUserInteractionEnabled == false` なので，UIButtonを子にしても動きません．
+        // imageView.isUserInteractionEnabled = true
+        
+        imageView.snp.makeConstraints { make in
+            // NOTE: この制約は実現できません
+            // let ratio: CGFloat = 16/9
+            // make.top.equalToSuperview()
+            // make.width.equalToSuperview()
+            // make.height.equalTo(imageView.snp.width).multipliedBy(ratio)
+            make.top.bottom.leading.trailing.equalToSuperview()
+        }
+        
+        return imageView
+    }
+    
+    private static func addShareButton(_ containerView: inout UIView) -> UIButton {
+        let shareButton = UIButton()
+        containerView.addSubview(shareButton)
+        
+        shareButton.backgroundColor = .yellow
+        shareButton.layer.cornerRadius = 20
+        shareButton.setTitle("Share", for: .normal)
+        shareButton.setTitleColor(.black, for: .normal)
+        shareButton.titleLabel?.font = UIFont.mainFont(ofSize: 14)
+        shareButton.titleLabel?.textAlignment = .center
+        shareButton.contentHorizontalAlignment = .center
+        shareButton.contentVerticalAlignment = .center
+        
+        shareButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview().inset(40)
+            make.height.equalTo(40)
+            make.bottom.equalToSuperview().offset(-20)
+        }
+        
+        return shareButton
+    }
+    
+    private static func addBackButton(_ containerView: inout UIView) -> UIButton {
+        let backButton = UIButton()
+        containerView.addSubview(backButton)
+        
+        let image = UIImage(named: "outline_keyboard_arrow_left_white_36pt_1x")
+        backButton.setImage(image, for: .normal)
+        
+        backButton.snp.makeConstraints { make in
+            make.width.height.equalTo(36)
+            make.top.equalToSuperview().offset(18)
+            make.left.equalToSuperview().offset(18)
+        }
+        
+        return backButton
+    }
+    
+    private static func addSaveButton(_ containerView: inout UIView) -> UIButton {
+        let saveButton = UIButton()
+        containerView.addSubview(saveButton)
+        
+        let image = UIImage(named: "baseline_save_alt_white_36pt_1x")
+        saveButton.setImage(image, for: .normal)
+        
+        saveButton.snp.makeConstraints { make in
+            make.width.height.equalTo(36)
+            make.top.equalToSuperview().offset(18)
+            make.right.equalToSuperview().offset(-18)
+        }
+        
+        return saveButton
     }
 }
