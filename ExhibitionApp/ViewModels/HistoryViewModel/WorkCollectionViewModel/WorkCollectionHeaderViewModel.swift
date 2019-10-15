40bcece87b12cf1d49f36319ab4f01b3ef9d452f
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2019-10-15 21:33:47 +0900

    Create WorkCollectionHeaderViewModel

---
+++ ExhibitionApp/ViewModels/HistoryViewModel/WorkCollectionViewModel/WorkCollectionHeaderViewModel.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct WorkCollectionHeaderViewModel {
+    var workHeaderLabelText: String {
+        return "スキャンした作品"
+    }
+}
--- ExhibitionApp/Views/HistoryViewController/WorkCollectionView/WorkCollectionHeaderView.swift
+++ ExhibitionApp/Views/HistoryViewController/WorkCollectionView/WorkCollectionHeaderView.swift
@@ -3,7 +3,7 @@ import SnapKit
 
 final class WorkCollectionHeaderView: UICollectionReusableView {
 
-    lazy private var workHeaderLabel = UILabel()
+    private let viewModel = WorkCollectionHeaderViewModel()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -16,19 +16,14 @@ final class WorkCollectionHeaderView: UICollectionReusableView {
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        setupView()
-        setupLayout()
-    }
-    
-    private func setupView(){
+        let workHeaderLabel = UILabel()
         self.addSubview(workHeaderLabel)
+        
         workHeaderLabel.font = UIFont.mainFont(ofSize: 24)
-        workHeaderLabel.text = "スキャンした作品"
+        workHeaderLabel.text = viewModel.workHeaderLabelText
         workHeaderLabel.textAlignment = .left
         workHeaderLabel.textColor = .white
-    }
-    
-    private func setupLayout(){
+        
         workHeaderLabel.snp.makeConstraints{ make -> Void in
             make.left.equalTo(16)
             make.centerY.equalToSuperview()
