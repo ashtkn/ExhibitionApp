bfc1c8ea5fd2b644254f445ffae7d324d2877ae0
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2019-11-07 23:37:51 +0900

    ARKit Scene (#101)
    * Create nodes
    
    * Add images
    
    * Add events
    
    * Introduce group ID
    
    * Move label node
    
    * Add hashimoto daiki
    
    * Create text node
    
    * Show some nodes
    
    * Create ShipNode
    
    * Delete unnecessary files
    
    * Fix bug
    
    * add 3d models and fonts
    
    * test commit
    
    * test commit
    
    * Remove font files
    
    * Add fonts
    
    * Beta 6 (#109)
    
    * add font files
    
    * add 3d models (#108)
    
    * Update authors information (#110)

---
+++ ExhibitionApp/SceneNodes/ShipNode.swift
@@ -0,0 +1,60 @@
+import SceneKit
+
+final class ShipNode: SCNNode {
+    
+    let originalPosition: SCNVector3
+    private(set) var hasMoved: Bool = false
+    
+    func move(to position: SCNVector3) {
+        self.hasMoved = true
+        super.position = position
+    }
+    
+    func moveToOriginalPosition() {
+        self.hasMoved = false
+        super.position = originalPosition
+    }
+    
+    init(gropuId name: String, width: CGFloat, originalPosition: SCNVector3 = .init()) {
+        // Configure current class
+        self.originalPosition = originalPosition
+        
+        // Confugure SuperClass
+        super.init()
+        
+        // Configure text node
+        let shipScene = SCNScene(named: "art.scnassets/ship.scn")!
+        let shipNode = shipScene.rootNode
+        
+        shipNode.name = name
+        renameChildNodes(name: name, children: shipNode.childNodes)
+        
+//        for childNode in shipNode.childNodes {
+//            // テクスチャを貼るなら再帰的に子ノードを探索し，テクスチャを貼る対象のノードにテクスチャをはる
+//            // childNode.geometry?.materials.append(SCNMaterial())
+//            // childNode.geometry?.materials.first?.diffuse.contents = image
+//        }
+        
+        let (min, max) = shipNode.boundingBox
+        let ratio = width / CGFloat(max.x - min.x)
+        shipNode.scale = SCNVector3(ratio, ratio, ratio)
+        
+        // Add children nodes
+        super.addChildNode(shipNode)
+
+        // Configure entire transform
+        super.position = originalPosition
+    }
+    
+    private func renameChildNodes(name: String, children nodes: [SCNNode]) {
+        if nodes.count == 0 { return }
+        for node in nodes {
+            node.name = name
+            renameChildNodes(name: name, children: node.childNodes)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
